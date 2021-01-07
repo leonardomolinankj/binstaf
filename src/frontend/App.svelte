817ecacf284a2913d73553bad02da464a83d4632
leonardomolinankj <a5681232@sina.com>
2021-01-08 04:25:02 +0800

    change for flaw on app.svelte

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
   import Main from "./components/Main.svelte";
-  export let name;
 </script>
 
-<div>
-  <h1>Hello {name}!</h1>
-  <Main />
-</div>
+<Main />
 
 <style lang="postcss" global>
   @tailwind base;
